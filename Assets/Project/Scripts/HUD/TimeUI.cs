b691b0ea49340b9a89d518e19545faef7d36598f
Jon-Gear <xanthosgardenstudios@gmail.com>
2025-06-20 16:48:31 +0200

    daytime is now shown in ui

--- Assets/Project/Scripts/HUD/TimeUI.cs
+++ Assets/Project/Scripts/HUD/TimeUI.cs
@@ -1,16 +1,27 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeUI : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI clockText;
     [SerializeField] TextMeshProUGUI weekDayText;
     [SerializeField] TextMeshProUGUI dayTimeText;
+    [SerializeField] Image DayIcon;
+
+    [Header("Daytime Icons")]
+
+    [SerializeField] Sprite morningIcon;
+    [SerializeField] Sprite sunriseIcon;
+    [SerializeField] Sprite noonIcon;
+    [SerializeField] Sprite afternoonIcon;
+    [SerializeField] Sprite eveningIcon;
+    [SerializeField] Sprite nightIcon;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,6 +30,7 @@ public class TimeUI : MonoBehaviour
         UpdateClock();
         UpdateWeekDay();
         UpdateDayTime();
+        UpdateIcon();
     }
 
     void UpdateClock()
@@ -35,5 +47,33 @@ public class TimeUI : MonoBehaviour
     void UpdateWeekDay()
     {
         weekDayText.text = TimeManager.Instance.ToString(TimeManager.Instance.GetWeekDay());
-    }    
+    }
+
+    void UpdateIcon()
+    {
+        var dayTime = TimeManager.Instance.GetDayTime();
+        /**/
+        switch (dayTime)
+        {
+            case DAYTIME.Morning:
+                DayIcon.sprite = morningIcon;
+                break;
+            case DAYTIME.Sunrise:
+                DayIcon.sprite = sunriseIcon;
+                break;
+            case DAYTIME.Noon:
+                DayIcon.sprite = noonIcon;
+                break;
+            case DAYTIME.Afternoon:
+                DayIcon.sprite = afternoonIcon;
+                break;
+            case DAYTIME.Evening:
+                DayIcon.sprite = eveningIcon;
+                break;
+            case DAYTIME.Night:
+                DayIcon.sprite = nightIcon;
+                break;
+        }
+        /**/
+    }
 }
