35c6772a6df0d8e631c456047954d6fd9baa8d94
Jon-Gear <zhangir.dev@gmail.com>
2026-07-19 20:40:02 +0200

    mandatory save

---
+++ Assets/_HeartOfPrince_Demo/Scripts/Presentation/Runtime/Inspectable.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace HeartOfPrince.Presentation
+{
+    public class Inspectable : MonoBehaviour
+    {
+        [SerializeField] private string inspectable;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+        
+        
+    }
+}
